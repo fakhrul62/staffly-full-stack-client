9fb83b40f922b76f8a56cb6af72eef097ba6932d
fakhrul62 <ifakhrul23@gmail.com>
2025-01-21 02:52:00 +0600

    footer added

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Root = () => {
     return (
         <div className="px-5 pb-5 pt-24">
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
 
         </div>
     );
