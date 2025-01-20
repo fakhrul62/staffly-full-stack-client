ba2c4984842f0e7f00661413cdbaea909b57d12d
fakhrul62 <ifakhrul23@gmail.com>
2025-01-20 22:19:41 +0600

    payment history added

--- src/pages/Dashboard/PaymentHistory.jsx
+++ src/pages/Dashboard/PaymentHistory.jsx
@@ -1,12 +1,126 @@
 import React from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../hooks/useAuth";
 
 const PaymentHistory = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: payHistory = [], refetch } = useQuery({
+    queryKey: ["payHistory"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payrolls/${user.email}`);
+      return res.data;
+    },
+  });
+
+  console.log(payHistory);
   return (
     <div>
       <div>
         <h2 className="text-2xl font-bold font-bebas tracking-widest text-zinc-900 inline-block ">
           Payment History
         </h2>
+      </div>
+      <div className="mt-20">
+        <div className="flex flex-col">
+          <div className="-m-1.5 overflow-x-auto">
+            <div className="p-1.5 min-w-full inline-block align-middle">
+              <div className="border rounded-lg overflow-hidden">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-start text-xs font-medium text-gray-500"
+                      >
+                        No.
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-start text-xs font-medium text-gray-500"
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-start text-xs font-medium text-gray-500"
+                      >
+                        Salary
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-start text-xs font-medium text-gray-500"
+                      >
+                        Month
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-start text-xs font-medium text-gray-500"
+                      >
+                        Year
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-start text-xs font-medium text-gray-500"
+                      >
+                        Payment Date
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-start text-xs font-medium text-gray-500"
+                      >
+                        Payment Date
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200">
+                    {payHistory.map((item, idx) => (
+                      <tr key={item._id}>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                          {idx + 1}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                          {item.employee.name}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                          {item.employee.salary}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                          {item.month}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                          {item.year}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                          {item.payment_date}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                        {item.payment_status === "pending" ? (
+                            <button 
+                              type="button" disabled
+                              className="px-4 py-2 bg-red-300 text-red-700 rounded-lg border border-red-700"
+                            >
+                              Pending
+                            </button>
+                          ) : (
+                            <button
+                              type="button"
+                              disabled
+                              className="px-4 py-2 bg-green-300 text-green-700 rounded-lg border border-green-700"
+                            >
+                              Paid
+                            </button>
+                          )}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
